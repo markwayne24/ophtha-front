32de22fb4e6cafe74ec79149c583def086c6a303
Mark Wayne <mwpulido24@gmail.com>
2017-09-14 19:49:20 +0800

    Updated management clinics

---
+++ src/pages/Management/Clinics/index.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { Breadcrumb } from 'antd';
+
+class Clinics extends Component{
+    render() {
+        return(
+            <div>
+                <Breadcrumb style={{ margin: '12px 0' }}>
+                    <Breadcrumb.Item>Management</Breadcrumb.Item>
+                    <Breadcrumb.Item>Clinics</Breadcrumb.Item>
+                </Breadcrumb>
+                <div style={{ padding: 24, background: '#fff', minHeight: 360 }}>
+                    
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Clinics;
